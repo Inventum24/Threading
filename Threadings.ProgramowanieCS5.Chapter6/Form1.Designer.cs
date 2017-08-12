e9d031d2fcbb93d5df8658db833b26dec39d0ef8
Piotr Nowak <piotrek.n@outlook.com>
2017-08-12 15:44:22 +0200

    Chapter 6

---
+++ Threadings.ProgramowanieCS5.Chapter6/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Threadings.ProgramowanieCS5.Chapter6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button_6_1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button_6_2 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button_6_3 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button_6_4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button_6_1);
+            this.groupBox1.Location = new System.Drawing.Point(52, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(198, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Równległe wykonywanie dużej ilość zadań";
+            // 
+            // button_6_1
+            // 
+            this.button_6_1.Location = new System.Drawing.Point(42, 52);
+            this.button_6_1.Name = "button_6_1";
+            this.button_6_1.Size = new System.Drawing.Size(123, 23);
+            this.button_6_1.TabIndex = 0;
+            this.button_6_1.Text = "6.1";
+            this.button_6_1.UseVisualStyleBackColor = true;
+            this.button_6_1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button_6_2);
+            this.groupBox2.Location = new System.Drawing.Point(52, 169);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Task zwaracający wartość";
+            // 
+            // button_6_2
+            // 
+            this.button_6_2.Location = new System.Drawing.Point(42, 52);
+            this.button_6_2.Name = "button_6_2";
+            this.button_6_2.Size = new System.Drawing.Size(123, 23);
+            this.button_6_2.TabIndex = 0;
+            this.button_6_2.Text = "6.2";
+            this.button_6_2.UseVisualStyleBackColor = true;
+            this.button_6_2.Click += new System.EventHandler(this.button_6_2_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button_6_3);
+            this.groupBox3.Location = new System.Drawing.Point(52, 286);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Przekazanie wartości do Taska";
+            // 
+            // button_6_3
+            // 
+            this.button_6_3.Location = new System.Drawing.Point(42, 43);
+            this.button_6_3.Name = "button_6_3";
+            this.button_6_3.Size = new System.Drawing.Size(123, 23);
+            this.button_6_3.TabIndex = 0;
+            this.button_6_3.Text = "6.3";
+            this.button_6_3.UseVisualStyleBackColor = true;
+            this.button_6_3.Click += new System.EventHandler(this.button_6_3_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button_6_4);
+            this.groupBox4.Location = new System.Drawing.Point(52, 408);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 100);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Synchronizacja zadań";
+            // 
+            // button_6_4
+            // 
+            this.button_6_4.Location = new System.Drawing.Point(42, 45);
+            this.button_6_4.Name = "button_6_4";
+            this.button_6_4.Size = new System.Drawing.Size(123, 23);
+            this.button_6_4.TabIndex = 0;
+            this.button_6_4.Text = "6.4";
+            this.button_6_4.UseVisualStyleBackColor = true;
+            this.button_6_4.Click += new System.EventHandler(this.button_6_4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 566);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button_6_1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button_6_2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button_6_3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button_6_4;
+    }
+}
+
---
+++ Threadings.ProgramowanieCS5.Chapter6/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Threadings.ProgramowanieCS5.Chapter6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Action a = () =>
+            {
+                Console.WriteLine("Start zadania nr " + Task.CurrentId);
+                Thread.SpinWait(new Random().Next(100000000));
+                Console.WriteLine("Koniec zadania nr " + Task.CurrentId);
+            };
+            //kod tworzący wątki, uruchamiający je i czekający na ich zakończenie
+            List<Task> listaZadan = new List<Task>();
+            for (int i = 0; i < 100; i++)
+            {
+                listaZadan.Add(new Task(a));
+            }
+            listaZadan.ForEach(t => t.Start());
+            listaZadan.ForEach(t => t.Wait());
+
+        }
+
+        private void button_6_2_Click(object sender, EventArgs e)
+        {
+            Task<String> t = new Task<String>(() => { return "Dzień dobry"; });
+            t.Start();
+            Console.WriteLine(t.Result);
+        }
+
+        private void button_6_3_Click(object sender, EventArgs e)
+        {
+            List<Task<int>> lista = new List<Task<int>>();
+
+            Console.WriteLine("Sprawdz, czy liczba pierwsza:");
+            int n = Int32.Parse(Console.ReadLine());
+
+            for (int i = 2; i < (int)Math.Sqrt(n); i++)
+            {
+                lista.Add(new Task<int>((j) =>
+                            {
+                                if (n % (int)j == 0)
+                                {
+                                    return (int)j;
+                                }
+                                else
+                                {
+                                    return 0;
+                                }
+                            }, i));
+            }
+
+            foreach (Task<int> t in lista) { t.Start(); }
+            foreach (Task<int> t in lista) { t.Wait(); }
+            bool pierwsza = true;
+            foreach (Task<int> t in lista)
+            {
+                if (t.Result != 0)
+                {
+                    Console.WriteLine("Liczba {0} dzieli się przez {1}.", n, t.Result);
+                    pierwsza = false;
+                }
+            }
+            if (pierwsza) Console.WriteLine("Liczba {0} jest liczbą pierwszą.", n);
+        }
+
+        private void button_6_4_Click(object sender, EventArgs e)
+        {
+            /*
+             * Metoda WaitAll każe bieżącemu wątkowi czekać na wykonanie
+             * wszystkich wymienionych w jej argumentach zadań, natomiast metoda WaitAny wznawia
+             * pracę wątku głównego po zakończeniu dowolnego ze wskazanych zadań
+             */
+
+            Task t1, t2, t3;
+            t1 = new Task(() => {
+                Thread.Sleep(1000); Console.WriteLine("zadanie t1 o identyfikatorze {0} zakończone", Task.CurrentId); });
+            t2 = new Task(() => {
+                Thread.Sleep(2000); Console.WriteLine("zadanie t2 o identyfikatorze {0} zakończone", Task.CurrentId); });
+            t3 = new Task(() => {
+                Thread.Sleep(3000); Console.WriteLine("zadanie t3 o identyfikatorze {0} zakończone", Task.CurrentId); });
+
+            t2.ContinueWith((t) => 
+                {
+                    Console.WriteLine("Zadanie o identyfikatorze {1} zostało wykonane po zakończeniu zadania t2 o identyfikatorze {0}", t.Id,Task.CurrentId);
+                });
+
+            Task[] zadania = { t1, t2, t3 };
+
+            foreach (Task t in zadania) t.Start();
+            foreach (Task t in zadania) t.Wait();
+        }
+
+        //t2.ContinueWith((t)=>{
+        //  Console.WriteLine("Zadanie o identyfikatorze {1} zostało wykonane po zakończeniu zadania t2 o identyfikatorze {0}", t.Id,Task.CurrentId);
+        //               });
+    
+    }
+}
